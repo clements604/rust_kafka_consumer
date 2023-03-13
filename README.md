6c5460fbce7920fcd64f1f1c80b4adecc24e9076
Josh Clements <obsidian67@protonmail.com>
2023-03-13 21:04:48 +1100

    Output to file v1 WIP

--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ## Reference:
     https://docs.rs/kafka/latest/kafka/consumer/index.html
     https://kafka.apache.org/quickstart
+
+### WIP Features
+    - Multiple topics allowed
+    - Output to file
+    - Command line arguments
+    - with_topic_partitions currently hardcoded to 0,1
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,21 @@ use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 use log::{info, warn, error, debug, trace, LevelFilter};
 use env_logger::Builder;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
+use std::fs::metadata;
 
 mod lib;
 
 fn main() {
 
-    let mut cfg_map: HashMap<String, String> = HashMap::new();
-
     Builder::new()
-        .filter_level(LevelFilter::Info)
+        .filter_level(LevelFilter::Debug)
         .init();
 
-        cfg_map = lib::load_cfg();
+    let cfg_map = lib::load_cfg();
 
-        debug!("{:?}", cfg_map["BOOTSTRAP_SERVERS"]);
+    debug!("{:?}", cfg_map["BOOTSTRAP_SERVERS"]);
 
     let mut consumer = match
     Consumer::from_hosts(vec!(cfg_map["BOOTSTRAP_SERVERS"].to_owned()))
@@ -34,16 +35,37 @@ fn main() {
             }
         };
 
-    info!("Consumer created");
+    
+
+    debug!("Consumer created");
+
     loop {
-        for ms in consumer.poll().unwrap().iter() {
-        for m in ms.messages() {
-                info!("{:?}", std::str::from_utf8(&m.value).unwrap());
+        for message_set in consumer.poll().unwrap().iter() {
+            for message in message_set.messages() {
+                info!("{:?}", std::str::from_utf8(&message.value).unwrap());
             }
-            consumer.consume_messageset(ms);
+            match consumer.consume_messageset(message_set) {
+                Ok(result) => result,
+                Err(why) => error!("{}", why)
+            };
         }
         consumer.commit_consumed().unwrap();
     }
 
 }
 
+fn write_message_to_file(topic: String, message: String){
+
+    match metadata(format!("{}.log", topic)) {
+        Ok(..) => { Ok },
+        Err(..) => {
+            match File::create(format!("{}.log", topic)) {
+                Ok(_) => { Ok },
+                Err(..) => {
+                    error!("Can't open file {}.log for writing", topic);
+                    Err
+                }
+            };
+        },
+    };
+}
