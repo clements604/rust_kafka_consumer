c664d53e3e180f478a5c2cad9995f70d240dd6d7
Josh Clements <clements604@gmail.com>
2023-04-09 20:47:54 +1000

    cleanup wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,45 @@
-use std::fs::{File, read_to_string, remove_file};
-use std::path::{Path, PathBuf};
+use std::fs::{File, read_to_string};
+use std::path::{Path};
 use std::io::prelude::*;
-use std::str::FromStr;
-use serde_json::{json, Result};
-use serde::{Deserialize, Serialize};
-use log::{info, warn, error, debug, trace, LevelFilter};
-use std::collections::HashMap;
+use serde_json::{json};
+use log::{error, debug};
 use std::process;
-use serde_json::Value;
 use std::fs::metadata;
 
 const DATA: &str = r#"{
-    "SSL_ENABLED": false,
-    "IDENTIFICATION_ALGORITHIM": "ssl.endpoint.identification.algorithm=",
-    "KEYMANAGER_ALGORITHIM": "ssl.keymanager.algorithm=SunX509",
-    "KEYSTORE_TYPE": "ssl.keystore.type=JKS",
-    "TRUSTMANAGER_ALGORITHIM": "ssl.trustmanager.algorithm=PKIX",
-    "TRUSTSTORE_TYPE": "ssl.truststore.type=JKS",
-    "SSL_ENABLED_PROTOCOLS": "ssl.enabled.protocols=TLSv1.2,TLSv1.1,TLSv1",
-    "SECURITY_PROTOCOL": "security.protocol=SSL",
-    "KEYSTORE_LOCATION": "ssl.keystore.location=kafka.keystore.jks",
-    "KEYSTORE_PASSWORD": "ssl.keystore.password=kafka@1234",
-    "TRUSTSTORE_LOCATION": "ssl.truststore.location=kafka.truststore.jks",
-    "TRUSTSTORE_PASSWORD": "ssl.truststore.password=kafka@1234",
-    "PRIVATE_KEY_PASSWORD": "ssl.key.password=kafka@1234",
     "BOOTSTRAP_SERVERS": "localhost:9092",
-    "AUTOCOMMIT_FLAG": "enable.auto.commit=false",
+    "AUTOCOMMIT_FLAG": false,
     "GROUP_ID": "",
-    "OFFSET_RESET_FLAG": "auto.offset.reset=earliest",
-    "TOPICS": "quickstart-events",
-    "BROKER_TIMEOUT": "default.api.timeout.ms=10000",
-    "MAX_POLL_RECORDS": "max.poll.records=100"
+    "OFFSET_RESET_FLAG": "earliest",
+    "TOPICS": "quickstart-events"
 }"#;
 
 pub const DEFAULT_PATH_STR: &str = "configuration.json";
 
-fn get_config_path() -> &'static Path {
-    Path::new(DEFAULT_PATH_STR)
-}
-
 pub fn load_cfg(file_path: Option<String>) -> serde_json::Value{
+    /*
+    *   Load configuration file, if file does not exist then create at the specified path.
+    *   Panics if file can't be loaded and if default can't created then loaded.
+    */
     debug!("load_cfg start");
     
-    let mut cfg_path = get_config_path();
+    let mut cfg_path = Path::new(DEFAULT_PATH_STR);
 
     if let Some(path_str) = file_path.as_deref(){
         cfg_path = Path::new(path_str);
     }
 
     match check_create_file(&cfg_path) {
+        Ok(_) => {
+            debug!("load_cfg finish");
+            let cfg_map = load_cfg_from_file(Path::new(DEFAULT_PATH_STR));
+            debug!("load_cfg finish");
+            return cfg_map;
+        },
         Err(why) => {
             error!("Error loading configuration file: {:?}", &why.to_string());
             //println!("{:?}", create_default_cfg());
             process::abort()
-        },
-        Ok(_) => {
-            debug!("load_cfg finish");
-            let cfg_map = load_cfg_from_file(get_config_path());
-            debug!("load_cfg finish");
-            return cfg_map;
         },
     };
 }
@@ -82,29 +63,15 @@ pub fn check_create_file(path: &Path) -> std::io::Result<()> {
     }
 }
 
-/*fn check_cfg_file(path: &Path) -> Result<String>  {
-    debug!("check_cfg_file start");
-    match metadata(path) {
-        Ok(..) => {
-            debug!("Configuration file exists, using existing file");
-            debug!("check_cfg_file finish");
-            Ok(String::from(""))
-        },
-        Err(..) => {
-            info!("Configuration file does NOT exist, creating default.");
-            create_default_cfg(String::from(DEFAULT_PATH_STR));
-            debug!("check_cfg_file finish");
-            Ok(String::from("Configuration file does NOT exist, creating default."))
-        }
-    }
-}*/
-
 fn create_default_cfg(path: String) -> std::io::Result<()> {
+    /*
+    *   Create default configuration file at specified path.
+    */
     debug!("create_default_cfg start");
 
     let data: serde_json::Value = serde_json::from_str(DATA).unwrap();
     
-    let mut file = match File::create(path) {
+    match File::create(path) {
         Ok(mut file) => {
             match file.write_all(data.to_string().as_bytes()) {
                 Ok(..) => {
@@ -122,6 +89,9 @@ fn create_default_cfg(path: String) -> std::io::Result<()> {
 }
 
 fn load_cfg_from_file(path: &Path) -> serde_json::Value {
+    /*
+    *   Loads existing configuration from file.
+    */
     debug!("load_cfg_from_file start");
 
     let file_contents: String = match read_to_string(path) {
@@ -149,38 +119,6 @@ fn load_cfg_from_file(path: &Path) -> serde_json::Value {
 
 }
 
-/*fn load_cfg_from_file(path: &Path) -> HashMap<String, String>{
-    debug!("load_cfg_from_file start");
-    let file_contents: String = match read_to_string(path) {
-        Ok(contents) => {
-            debug!("{}", contents);
-            contents
-        },
-        Err(why) => {
-            error!("{}", why);
-            String::from("")
-        },
-    };
-    let json_result: Result<Value, > = serde_json::from_str(&file_contents);
-    let cfg_json: Value = match json_result{
-        Ok(json_val) => json_val,
-        Err(why) => {
-            error!("{}", why);
-            serde_json::from_str("{}").unwrap()
-        }
-    };
-    let mut cfg_map: HashMap<String, String> = HashMap::new();
-    if let Some(temp) = cfg_json.as_object() {
-        for(key, value) in temp.iter(){
-            debug!("key [{:?}], value [{:?}]", key, String::from(value.as_str().unwrap_or("")));
-            cfg_map.insert(String::from(key), String::from(value.as_str().unwrap_or("")));
-        }
-    }
-    debug!("load_cfg_from_file finish");
-    return cfg_map;
-}*/
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -190,37 +128,37 @@ mod tests {
     fn test_check_create_file_create_default() {
         // !!! WARNING !!!
         // Running this test will override current configuration.json file
-        /*  Runs check_create_file with no configuration file present to test file creation */
-        let _ = remove_file(DEFAULT_PATH_STR);
+        /*  Test test_check_create_file_create_default function */
+        let _ = std::fs::remove_file(Path::new(DEFAULT_PATH_STR));
         assert!(check_create_file(Path::new(DEFAULT_PATH_STR)).is_ok());
-        let config_map = load_cfg_from_file(get_config_path());
+        let config_map = load_cfg_from_file(Path::new(DEFAULT_PATH_STR));
         assert_eq!(config_map.is_null(), false);
-        assert_eq!(config_map["SSL_ENABLED"], false);
+        assert_eq!(config_map["AUTOCOMMIT_FLAG"].as_bool().unwrap(), false);
     }
 
     #[test]
     fn test_check_create_file_existing() {
-        /*  Runs check_create_file with existing configuration file present */
+        /*  Test test_check_create_file_existing function */
         assert!(check_create_file(Path::new(DEFAULT_PATH_STR)).is_ok());
         let config_map: serde_json::Value = load_cfg(Some(String::from(DEFAULT_PATH_STR)));
-        println!("{:?}", config_map["PRIVATE_KEY_PASSWORD"].as_str());
         assert_eq!(config_map.is_null(), false);
-        assert_eq!(config_map["SSL_ENABLED"], false);
+        assert_eq!(config_map["AUTOCOMMIT_FLAG"].as_bool().unwrap(), false);
     }
 
     #[test]
-    fn test_get_config_path_existing() {
-        let config_map = load_cfg_from_file(get_config_path());
+    fn test_get_default_config_path_existing() {
+        /*  Test load_cfg_from_file function */
+        let config_map = load_cfg_from_file(Path::new(DEFAULT_PATH_STR));
         assert_eq!(config_map.is_null(), false);
-        println!("###########\t{}", config_map["SSL_ENABLED"]);
-        assert_eq!(config_map["SSL_ENABLED"], false);
+        assert_eq!(config_map["AUTOCOMMIT_FLAG"].as_bool().unwrap(), false);
     }
 
     #[test]
     fn test_load_cfg_existing() {
+        /*  Test test_load_cfg_existing function */
         let config_map: serde_json::Value = load_cfg(Some(String::from(DEFAULT_PATH_STR)));
         assert_eq!(config_map.is_null(), false);
-        assert_eq!(config_map["SSL_ENABLED"], false);
+        assert_eq!(config_map["AUTOCOMMIT_FLAG"].as_bool().unwrap(), false);
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,18 @@
-use kafka::consumer;
 use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
-use log::{info, warn, error, debug, trace, LevelFilter};
+use log::{info, error, debug, LevelFilter};
 use env_logger::Builder;
-use std::collections::HashMap;
-use std::fs::File;
-use std::hash::Hash;
 use std::io::prelude::*;
-use std::fs::metadata;
 use std::fs::OpenOptions;
-use kafka::error::{Error};
 use serde_json::Value;
 
 mod lib;
 mod utils;
 
 mod args;
-use args::AppArgs;
-use clap::{Parser, App, Arg};
+use clap::{App, Arg};
 
 fn main() {
 
-    //let args = AppArgs::parse();
     let mut commit_consumed: bool = false;
     let mut message_key: String = String::from("");
 
@@ -115,6 +107,9 @@ fn main() {
     if matches.is_present("auto_commt") {
         commit_consumed = true;
     }
+    else {
+        commit_consumed = cfg_map["AUTOCOMMIT_FLAG"].as_bool().unwrap_or(false);
+    }
 
     if matches.is_present("message_key") {
         message_key = String::from(matches.value_of("message_key").unwrap_or(""));
@@ -127,6 +122,7 @@ fn main() {
     }
 
     let mut consumer = get_consumer(&cfg_map);
+
     debug!("Consumer created");
 
     loop {
@@ -135,7 +131,6 @@ fn main() {
             for message in message_set.messages() {
                 if std::str::from_utf8(&message.key).unwrap() == &message_key || &message_key == "" {
                     let mut message: String = String::from(std::str::from_utf8(&message.value).unwrap());
-                    //debug!("{:?}", message);
                     let timestamp = utils::get_timestamp();
                     message = timestamp.to_owned() + "\t\t" + &message;
                     if file_output {
@@ -152,7 +147,7 @@ fn main() {
                 Err(why) => error!("{}", why)
             };
         }
-        if commit_consumed {
+        if commit_consumed && cfg_map["GROUP_ID"].as_str().unwrap_or("") != "" {
             consumer.commit_consumed().unwrap();
         }
     }
@@ -177,12 +172,18 @@ fn write_message_to_file(topic: String, message: String) {
 }
 
 fn get_consumer(cfg_map: &serde_json::Value) -> Consumer{
+
     let bootstrap_servers: Vec<String> = vec!(cfg_map["BOOTSTRAP_SERVERS"].as_str().unwrap_or("").split(",").collect());
+
     let mut consumer = Consumer::from_hosts(bootstrap_servers)
-    //.with_topic_partitions(cfg_map["TOPICS"].to_owned(), &[0, 1])
-    .with_fallback_offset(FetchOffset::Earliest)
     .with_group(cfg_map["GROUP_ID"].to_string().to_owned())
     .with_offset_storage(GroupOffsetStorage::Kafka);
+
+    match cfg_map["OFFSET_RESET_FLAG"].as_str().unwrap_or("") {
+        "earliest" => consumer = consumer.with_fallback_offset(FetchOffset::Earliest),
+        "latest" => consumer = consumer.with_fallback_offset(FetchOffset::Latest),
+        _ => consumer = consumer.with_fallback_offset(FetchOffset::Earliest),
+    };
 
     let topics: Vec<&str> = cfg_map["TOPICS"].as_str().unwrap_or("").split(",").collect();
     debug!("{:?}", topics);
@@ -190,7 +191,6 @@ fn get_consumer(cfg_map: &serde_json::Value) -> Consumer{
     for topic in topics {
         debug!("{}", topic);
         consumer = consumer.with_topic(topic.to_owned())
-        //.with_topic_partitions(topic.to_owned(), &[0, 1]);
     }
 
     match consumer.create() {
